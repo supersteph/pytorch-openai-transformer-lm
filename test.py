da7542d92ebb716a2dba332df5404e7cdd3316ff
Stephen Wu <stephenwu@StephendeMacBook-Pro.local>
2019-09-16 19:54:51 -0700

    fixed indents

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ start = dt.datetime.now()
 t = dt.datetime.now()
 i = 0
 while True:
-  delta=dt.datetime.now()-t
-  if delta.seconds >= 60:
-  	 i+=1
-     print(i)
-     t = dt.datetime.now()
+	delta=dt.datetime.now()-t
+	if delta.seconds >= 60:
+		i+=1
+		print(i)
+		t = dt.datetime.now()
